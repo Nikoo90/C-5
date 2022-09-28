8857772f700d6e0597c57ed2dfc15b54cd7816e9
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-28 21:27:46 +0300

    the number of even numbers in the array

---
+++ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int[] createArr(int lengthArr)
+{
+    int[] arr = new int[lengthArr];
+    for (int i = 0; i < lengthArr; i++)
+    {
+        arr[i] = new Random().Next(1, 1000);
+    }
+    return arr;
+}
+
+int evenNumbersArr(int[] arr)
+{
+    int count = 0;
+    foreach (int element in arr)
+    {
+        if (element % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+
+Console.WriteLine(evenNumbersArr(createArr(8)));
