9cab9604515799477eace243792ba81494cb8c45
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-28 21:49:23 +0300

    sum of elements in odd positions

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] createArr(int lengthArr)
+{
+    int[] arr = new int[lengthArr];
+    for (int i = 0; i < lengthArr; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+int sumElement(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
+Console.WriteLine(sumElement(createArr(7)));
